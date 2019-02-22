7a66bb19d10c050fc16726d36019d72b9464ada3
ShengzW <shengz.wg@gmail.com>
2019-02-21 23:56:22 -0800

    commit message

---
+++ public/js/rate-recipe.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    $("a#rate-left").click(rateLeft);
+    $("a#rate-right").click(rateRight);
+}
+
+
+function rateLeft(e) { 
+    e.preventDefault();
+    $(this).css('border','2 solid gray');
+    $("#rate-right").css('border','0');
+}
+
+function rateRight(e) { 
+    e.preventDefault();
+    $(this).css('border','2 solid gray');
+    $("#rate-left").css('border','0');
+}
