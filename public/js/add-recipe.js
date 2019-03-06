2b9010bcff34a85c09f140d274a07fcf79d2c076
ShengzW <shengz.wg@gmail.com>
2019-03-05 18:38:25 -0800

    serving size

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -77,12 +77,14 @@ $.fn.scrollView = function () {
 function addRecipe() {
     if ($('#recipe-name-edit').val() == '') { $('#recipe-name-loc').scrollView(); $('#recipe-name-edit').css('border', 'solid red 1px'); return;} else { $('#recipe-name-edit').css('border', 'solid 0 black'); }
     if ($('#nre-cooking-instructions').val() == '') { $('#nre-cooking-instructions-loc').scrollView(); $('#nre-cooking-instructions').css('border', 'solid red 1px'); return;} else { $('#nre-cooking-instructions').css('border', 'solid 0 black'); }
+    if ($('#nre-cooking-time').val() == '') { $('#nre-cooking-time').scrollView(); $('#nre-cooking-time').css('border', 'solid red 1px'); return;} else { $('#nre-cooking-time').css('border', 'solid 0 black'); }
+    if ($('#nre-serving-size').val() == '') { $('#nre-serving-size').scrollView(); $('#nre-serving-size').css('border', 'solid red 1px'); return;} else { $('#nre-serving-size').css('border', 'solid 0 black'); }
     var rcpName = $('#recipe-name-edit').val();
     var recipeID = Math.floor(Math.random() * (1000000-9000000) + 1000000);
     var image = "/images/user-recipe.jpg";
     var ingredients = foods;
     var time = $('#nre-cooking-time').val();
-    var serving = 1;
+    var serving = $('#nre-serving-size').val();;
     var instructions = $('#nre-cooking-instructions').val(); 
 
     var json_data = {
