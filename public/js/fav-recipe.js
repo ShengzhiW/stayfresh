767199f9204bc52cb0cc259f2f5eceb45dcea059
ShengzW <shengz.wg@gmail.com>
2019-03-07 07:47:44 -0800

    commit message

--- public/js/fav-recipe.js
+++ public/js/fav-recipe.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     idStatus = $('#id_stats').val();
     if (favStatus == "true") {
         $("#rcp-fav").css({"background": "#FC0041", "color": "white"})
-        $("#rcp-fav").html("❤<br>Favorited")
+        $("#rcp-fav").html("<img src='/images/fav-button.png' width='20px'><br>Favorited")
     }
     $("#rcp-fav").click(favRecipe);
 })
@@ -17,12 +17,12 @@ function favRecipe() {
     if (favStatus == "true"){
         favStatus = "false";
         $("#rcp-fav").css({"background": "#F2F2F2", "color": "#FC0041"})
-        $("#rcp-fav").html("❤<br>Add to Favorites")
+        $("#rcp-fav").html("<img src='/images/fav-button-pink.png' width='20px'><br>Add to Favorites")
     }
     else{
         favStatus = "true";
         $("#rcp-fav").css({"background": "#FC0041", "color": "white"})
-        $("#rcp-fav").html("❤<br>Favorited")
+        $("#rcp-fav").html("<img src='/images/fav-button.png' width='20px'><br>Favorited")
     }
 
     var json_data = {
